db61cd5b07fe095fbfaeed3443803cff7b0c3838
ksj <jshhoon2004@naver.com>
2018-10-11 17:44:40 +0900

    added World is Mine

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace GitTutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("World is Mine");
         }
     }
 }
