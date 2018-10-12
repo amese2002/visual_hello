fe155d7e8a7c097b15102d6ae3cb4b06a1507312
amese2002 <44048368+amese2002@users.noreply.github.com>
2018-10-12 15:20:01 +0900

    Enemy 25 points

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace GitTutorial
             Console.WriteLine("Battelion Major, Ready for Action");
             Console.WriteLine("Enemy down 100 points");
             Console.WriteLine("Keep up the Press. They down 50 points");
+            Console.WriteLine("Enemy down 25 points.");
         }
     }
 }
